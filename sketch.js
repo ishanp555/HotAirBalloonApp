4375309900a80644df30efe30d6042e75d049224
ishanp555 <77009831+ishanp555@users.noreply.github.com>
2021-04-30 10:54:20 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
 var balloon,balloonImage1,balloonImage2;
-var position ;
 // create database and position variable here
 
 function preload(){
@@ -19,9 +18,6 @@ function setup() {
   balloon.addAnimation("hotAirBalloon",balloonImage1);
   balloon.scale=0.5;
 
-  var ballonPosition=database.ref('ballon/height')
-  ballonPosition.on("value",readPosition,showError)
-
   textSize(20); 
 }
 
@@ -31,21 +27,26 @@ function draw() {
 
   if(keyDown(LEFT_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.x=balloon.x-5;
     //write code to move air balloon in left direction
+  
   }
   else if(keyDown(RIGHT_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in right direction
+    balloon.x=balloon.x+5;
   }
   else if(keyDown(UP_ARROW)){
-    updateHeight(0,-10)
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    ballo0n.scale = balloon.scale -0.01
     //write code to move air balloon in up direction
+    balloon.y=balloon.y-5;
+    balloon.scale=balloon.scale-0.002;
   }
   else if(keyDown(DOWN_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in down direction
+    balloon.y=balloon.y+5;
+    balloon.scale=balloon.scale+0.002;
   }
 
   drawSprites();
@@ -54,19 +55,3 @@ function draw() {
   textSize(25);
   text("**Use arrow keys to move Hot Air Balloon!",40,40);
 }
-
-function updateHeight (x,y){
-  database.ref('ballon/height').set({
-    'x': height . x + x ,
-    'y': height . y + y
-  })
-
-}
-
-function readHeight (data){
-  height = data.val();
-
-  ballon.x = height . x ;
-  ballon.y = height . y;
-}
-
